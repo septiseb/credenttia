dd9d2481623e13615c0b6d504ce4bc39b7c84f2b
Sebastian Septien <sebsepdus@hotmail.com>
2021-04-16 14:03:08 -0500

    Styles was changed to Tailwind

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { ChakraProvider, resetCSS } from "@chakra-ui/react"
-import Navbar from './components/Navbar.tsx'
-import MainPage from './components/MainPage';
+import Navbar from "./components/Navbar.jsx";
+import MainPage from "./components/MainPage"
 
 function App() {
   return (
-    <ChakraProvider>
-    <resetCSS />
-    <Navbar />
-      <MainPage />      
-      </ChakraProvider>
+    <>
+      <Navbar />
+     <MainPage /> 
+    </>
   );
 }
 
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainSection from "./Landing/MainSection";
+import Footer from "./Landing/Footer";
+
+export default function MainPage() {
+  return (
+    <>
+      <MainSection />
+      <Footer />
+    </>
+  );
+}
